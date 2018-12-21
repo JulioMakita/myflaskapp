0d6fbbb1774bed5f938e332d35c16f6af5ed99a2
JulioMakita <juliobrjp@hotmail.com>
2018-12-21 12:28:22 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,179 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, SelectField, TextAreaField, PasswordField, validators, StringField
+from passlib.hash import sha256_crypt
+from functools import wraps
+
+app = Flask(__name__)
+Articles = Articles()
+
+app.secret_key = "12345"
+
+#Config MySQL
+app.config['MYSQL_HOST'] = '172.17.0.3'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'CdgftfpskR828sqz'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+#init MySQL
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = Articles)
+
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html', id = id)
+
+@app.route('/docker')
+def dockerTutorial():
+    return render_template('/tutorials/docker.html')
+
+@app.route('/kafka')
+def kafkaTutorial():
+    return render_template('/tutorials/kafka.html')
+
+@app.route('/python')
+def pythonTutorial():
+    return render_template('/tutorials/python.html')
+
+@app.route('/saveArticle')
+def saveArticle():
+    return "hello"
+
+
+class RegisterForm(Form):
+    name = StringField('Name', validators=[validators.Length(min=1, max=50)])
+    username = StringField('Username', validators=[validators.Length(min=4, max=25)])
+    email = StringField('Email', validators=[validators.Length(min=6, max=50)])
+    password = PasswordField('Password', 
+        validators=[validators.DataRequired(), 
+        validators.EqualTo('confirm', message='Password does not match')
+    ])
+
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(form.password.data)
+
+        #Create cursor
+        cur = mysql.connection.cursor()
+
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        #Commit to DB
+        mysql.connection.commit()
+
+        cur.close()
+
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('index'))
+        
+    return render_template('register.html', form = form)
+
+#User Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        #Create cursor
+        cur = mysql.connection.cursor()
+
+        #Get username 
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            #Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            #Compare passwords
+            if sha256_crypt.verify(password_candidate, password):
+                #Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard')) 
+            else:
+                error = 'Invalid Login'
+                return render_template('login.html', error=error)
+            #Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error= error)
+
+    return render_template('login.html')     
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+#Check if user is logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
+
+#Article form class
+class ArticleForm(Form):
+    title = StringField('Title', validators=[validators.Length(min=1, max=200)])
+    body = TextAreaField('Body', validators=[validators.Length(min=30)])
+
+@app.route('/add_article', methods=['GET', 'POST'])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method =='POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        cur = mysql.connection.cursor()
+
+        cur.execute("INSERT INTO articles(title, body, author) VALUES(%s, %s, %s)", (title, body, session['username'])) 
+
+        mysql.connection.commit()
+
+        cur.close()
+
+        flash('Article Created', 'success')
+
+        return render_template('dashboard.html')
+    return render_template('add_article.html', form = form)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=2020)
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title': 'Article One',
+            'body': 'Lorem   asaswwqw',
+            'author': 'Brad New',
+            'create_date': '04-25-2017'
+        },
+                {
+            'id':2,
+            'title': 'ArticleTwo One',
+            'body': 'Lorem   asaswwqw',
+            'author': 'Paulo New',
+            'create_date': '04-25-2017'
+        },
+                {
+            'id':31,
+            'title': 'Article Three',
+            'body': 'Lorem   asaswwsddsqw',
+            'author': 'Rick New',
+            'create_date': '04-25-2017'
+        }
+    ]
+    return articles
